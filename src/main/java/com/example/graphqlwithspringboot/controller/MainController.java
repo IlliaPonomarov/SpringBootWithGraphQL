7fd30df65b2f6ca2ede07456c1c3bec869dc26ba
Illia <chippaxyucac@gmail.com>
2022-08-17 14:42:38 +0200

    Maven Dependecies was changed

--- src/main/java/com/example/graphqlwithspringboot/controller/MainController.java
+++ src/main/java/com/example/graphqlwithspringboot/controller/MainController.java
@@ -1,4 +1,17 @@
 package com.example.graphqlwithspringboot.controller;
 
+import com.example.graphqlwithspringboot.model.Book;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MainController {
+
+
+    @QueryMapping
+    public Book bookById(@Argument String id){
+
+    }
 }
